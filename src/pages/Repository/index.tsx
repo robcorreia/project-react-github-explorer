dad2a62e3d14f8a61163ec2b0cc762b8c45068c7
Robson <robccorreia@gmail.com>
2022-03-17 16:16:10 -0300

    first commit, init project

---
+++ src/pages/Repository/index.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/function-component-definition */
+import React from 'react';
+
+const Repository: React.FC = () => {
+  return <h1>Repository</h1>;
+};
+
+export default Repository;
